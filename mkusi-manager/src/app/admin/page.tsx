cccbf9344e6804813cbdc87a75452023d80ba34b
miyevu <keklemyevu@gmail.com>
2026-02-24 20:06:52 +0100

    preview deployment

--- mkusi-manager/src/app/admin/page.tsx
+++ mkusi-manager/src/app/admin/page.tsx
@@ -231,7 +231,7 @@ export default function AdminDashboard() {
                 series={[{ data: chartData, color: '#1e293b' }]}
                 height={350}
                 margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
-                hideLegend // <-- The new v8 way to hide the legend
+                hideLegend 
                 borderRadius={8}
               />
             </Box>
